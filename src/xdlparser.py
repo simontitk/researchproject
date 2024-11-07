349eac45011a86bc3f5073c38bfa456cf1046b4c
simontitk <simontitk@gmail.com>
2024-11-07 23:30:01 +0100

    implement parsing logic

--- src/xdlparser.py
+++ src/xdlparser.py
@@ -1,29 +1,64 @@
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
 from instructions import InstructionMapper, Instruction, StartInstruction
+
+class Node:
+    def __init__(self, instruction: Instruction) -> None:
+        self.instruction = instruction
+
+    
+    def __repr__(self) -> str:
+        name = self.instruction.__class__.__name__
+        return f"Node({name}, {self.status}, queue={self.instruction.queue})"
+
 
 class XDLParser:
 
     def __init__(self) -> None:
-        self.queues: dict[str, Instruction] = {
-            None: StartInstruction()
-        }
+        self.mapper = InstructionMapper()
+        self.graph = StartInstruction()
+        self.queues: dict[str, Instruction] = {} 
 
-
+    def get_queue(self, e: Element):
+        return e.attrib.get("queue", None)
+    
+    def has_queues(self):
+        return bool(self.queues.keys())
+ 
+    
     def parse(self, path: str):
+        root_head = self.graph
         tree = ET.parse(path)
         root = tree.getroot()
 
         for child in root:
-            inst = InstructionMapper.map(instruction=child.tag, **child.attrib)
-            
-            """
-            TODO: what types of synthesis/example processes to expect?
-            
-            """
 
-        return self.queues[None]
+            q = self.get_queue(child)
+            i = self.mapper.map(child.tag, **child.attrib)
+
+            if q is None:
+                if not self.has_queues():
+                    root_head.add_child(i)
+                    print(i, "goes to root q")
+                else:
+                    for qn in self.queues.values():
+                        qn.add_child(i)
+                    self.queues = {}
+                    print(i, "closes queues")
+                root_head = i
+
+            else:
+                if q not in self.queues:
+                    self.queues[q] = i
+                    root_head.add_child(i)
+                    print(i, "starts queue")
+                else:
+                    self.queues[q].add_child(i)
+                    self.queues[q] = i
+                    print(i, "builds queue")
+        return self.graph
 
 
 if __name__ == "__main__":
-    parser = XDLParser()
-    xdl_dag = parser.parse("data/example.xml")
+    p = XDLParser()
+    DAG = p.parse("data/example.xml")
