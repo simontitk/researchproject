e32b7f382f23db9f20afb5a65250c1bafdaec890
simontitk <simontitk@gmail.com>
2024-11-07 23:33:46 +0100

    add server and post request

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,51 +1,56 @@
 from enum import StrEnum
+import requests
+from flask import Flask, jsonify, request, render_template
+import os
+
 
 class Orchestrator:
-    ...
+    """
+    - receives the DAG representation of the synthesis process as input
+    - starts at the StartInstruction of the DAG and processes the subsequent ones:
+        - fetch instruction
+        - check if all dependencies are fulfilled
+        - send out request to specified tile
+        - poll the tile for fulfilment (?)
+        - go to next instruction
+    """
 
-"""
-- receives the DAG representation of the synthesis process as input
-- starts at the StartInstruction of the DAG and processes the subsequent ones:
-    - fetch instruction
-    - check if all dependencies are fulfilled
-    - send out request to specified tile
-    - poll the tile for fulfilment (?)
-    - go to next instruction
-
-- implemented as aiohttp client 
-
-"""
-
-class TileStates(StrEnum):
-    READY = "ready"
-    BUSY = "busy"
-    ERROR = "error"
+    def __init__(self, port: int) -> None:
+        self.server = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'))
+        self.port = port
+        self._add_routes()
 
 
-class Tile:
-    def __init__(self, ip) -> None:
-        self.status = TileStates.READY
-        self.ip = ip
-    
-    def execute(self, args):
-        """Method overridden in each subclass for its corresponding instructions"""
-        self.status = TileStates.BUSY
-        ... # mocking execution logic with some probability of error
+    def _add_routes(self):
+
+        @self.server.route("/")
+        def index():
+            return render_template("overview.html")
 
 
+        @self.server.route("/send")
+        def send():
+            self.send_instruction("http://localhost:5001/execute")
+            return "", 200
+        
 
-class HeatingTile(Tile):
-    ...
+        @self.server.route("/callback", methods=["POST"])
+        def callback():
+            return 
 
-class UvTile(Tile):
-    ...
 
-"""
-- implemented as REST API servers using Flask/FastAPI
-- basic Tile class and specified subclasses
-- can provide status infor:
-    - ready: if free to take instructions
-    - busy: if in the process of executing an instruction
-    - error: if sensors detect a problem / faces network issues
+    def send_instruction(self, address):
+        data = {
+            "callback_url": f"http://localhost:{self.port}/callback",
+            "task": "do it",
+        }
+        response = requests.post(address, json=data)
 
-"""
+
+    def run(self):
+        self.server.run(port=self.port, debug=True)
+
+
+if __name__ == "__main__":
+    o = Orchestrator(port=5000)
+    o.run()
